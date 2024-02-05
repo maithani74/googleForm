1d60cf8913a8afccf429aa17771970b4beaab541
maithani74 <maithanir74@gmail.com>
2024-02-05 15:14:38 +0530

    last commit

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const express  =require("express");
 const mongoose = require("mongoose")
 const app = express();
 const morgan = require("morgan")
-const cors = require("cors")
+const cors = require('cors')
 const formRouter = require("./formRoute")
+const path = require('path')
 app.use(express.json());
 app.use(morgan("dev"))
 app.use(cors());
+app.use(express.static(path.join(__dirname,'../frontend/build')))
 const connectDB = async()=>{
     try {
         const con = await mongoose.connect("mongodb://localhost:27017/form");
@@ -19,6 +21,10 @@ const connectDB = async()=>{
 connectDB()
 
 app.use("/api/v1",formRouter.router)
+
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,"../frontend/build/index.html"))
+})
 
 app.listen(8080,()=>{
     console.log("Server started on port 8080")
